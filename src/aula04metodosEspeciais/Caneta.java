33e8991aa5cf337084c3e7c70448f853461ab75c
Thiago Neves <thiagonevestg0@gmail.com>
2025-09-28 14:02:06 -0300

    Atualização do codigo

--- src/aula04metodosEspeciais/Caneta.java
+++ src/aula04metodosEspeciais/Caneta.java
@@ -1,14 +1,27 @@
 package aula04metodosEspeciais;
 
+import java.sql.SQLOutput;
+
 public class Caneta {
     public String modelo;
     public String cor;
     private double ponta;
+    private boolean tampa;
 
     // Construtor
+    public void Caneta(String m, String c, Double p){
         setModelo(m);
         setCor(c);
         setPonta(p);
+        tampar();
+    }
+
+    public void status(){
+        System.out.println("SOBRE A CANETA: ");
+        System.out.println("Modelo: " +getModelo());
+        System.out.println("Cor: " +getCor());
+        System.out.println("Ponta: " +getPonta());
+        System.out.println("Tampada: " +tampa);
     }
 
     public String getModelo(){
@@ -34,4 +47,13 @@ public class Caneta {
     private void setPonta(double p){
         this.ponta = p;
     }
+
+    public void tampar(){
+        this.tampa = true;
+    }
+
+    public void destampar(){
+        this.tampa = false;
+    }
+
 }
